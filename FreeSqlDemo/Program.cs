3ff29f5cbc6f8965400c740443a6edd16a9f12c7
frank <1055560667@qq.com>
2021-08-06 08:38:21 +0800

    添加项目文件。

---
+++ FreeSqlDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FreeSqlDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
